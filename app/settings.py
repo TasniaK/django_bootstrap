58c74042286d9bf8a6bd1496ead91a20934466a6
Tasnia <tasniakamal@hotmail.co.uk>
2018-06-03 23:37:32 +0100

    Edited settings.py allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@4u!dc-0^10a%t9(rdpmci5vi4)*fskkj*-(+r^(a3q5#37b8k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [u'0.0.0.0', u'tasniakamal.herokuapp.com']
+ALLOWED_HOSTS = [u'0.0.0.0', u'tasniakamal.co.uk']
 
 
 # Application definition
