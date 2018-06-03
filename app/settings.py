0975c85cfe7dd76e38287d0fd41018bf06d1848f
Tasnia <tasniakamal@hotmail.co.uk>
2018-06-04 00:15:19 +0100

    Edited settings.py allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@4u!dc-0^10a%t9(rdpmci5vi4)*fskkj*-(+r^(a3q5#37b8k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [u'0.0.0.0', u'tasniakamal.co.uk']
+ALLOWED_HOSTS = [u'0.0.0.0', u'www.tasniakamal.co.uk']
 
 
 # Application definition
